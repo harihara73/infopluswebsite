3993fe00660fa6ecc724563738f66673bfd2f90a
harihara73 <92078960+harihara73@users.noreply.github.com>
2024-05-21 09:14:38 +0530

    Add files via upload

--- my js.js
+++ my js.js
@@ -1,27 +1,4 @@
 
-
-/*let vid1 = document.getElementById("myVideo1"); 
-
-function playVid1() { 
-  vid1.play(); 
-} 
-let vid2 = document.getElementById("myVideo2"); 
-
-function playVid2() { 
-  vid2.play(); 
-} 
-let vid3 = document.getElementById("myVideo3"); 
-
-function playVid3() { 
-  vid3.play(); 
-} */
-
-/* Get the element you want displayed in fullscreen */ 
-
-
-
- 
-
 const navE1 = document.querySelector('.navbar');
 
     window.addEventListener('scroll', () => {
@@ -81,44 +58,11 @@ const navE1 = document.querySelector('.navbar');
       sidebar.style.display = 'none';
     }
    
-
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: '#navbar-example'
+    })
     
   
 
-    $(document).ready(function(){
-      // Add smooth scrolling to all links in navbar + footer link
-      $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
-    
-          // Store hash
-          var hash = this.hash;
-    
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 900, function(){
-       
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-        } // End if
-      });
       
-      $(window).scroll(function() {
-        $(".slideanim").each(function(){
-          var pos = $(this).offset().top;
-    
-          var winTop = $(window).scrollTop();
-            if (pos < winTop + 600) {
-              $(this).addClass("slide");
-            }
-        });
-      });
-    })
-
-    
     
